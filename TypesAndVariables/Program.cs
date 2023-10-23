4cdd3a73f2daf124e5f849f84b86f5c1b725e710
Burak Tan <buraaktan78@gmail.com>
2023-10-24 01:56:43 +0300

    DataTypes

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -1,2 +1,47 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//VALUE TYPES
+
+//INT 
+int number1 = 2147483647; // Alabileceği maksimum sayıdır.(32 bit)
+Console.WriteLine("Number1 is {0}",number1);
+
+// LONG
+long number2 = 9223372036854775807; // Alabileceği maksimum sayıdır.(64 bit)
+Console.WriteLine("Number2 is {0}", number2);
+
+// SHORT
+short number3 = 32767; // Alabileceği maksimum sayıdır.
+Console.WriteLine("Number3 is {0}", number3);
+
+//BYTE
+byte number4 = 255; // Alabileceği maksimum sayıdır.
+Console.WriteLine("Number4 is {0}", number4);
+
+//BOOL
+bool condition=false; //True veya False olarak döner.
+Console.WriteLine(condition);
+
+//CHAR
+char charecter = 'A'; // Tek bir karakter tutmamıza yardımcı olur.
+Console.WriteLine(charecter);
+Console.WriteLine("Character is : {0}",(int)charecter); // Karakterin klavyeden almış olduğu ASCII numarasını yazdırmış olduk.
+
+//DOUBLE
+double number5 = 10.07;
+Console.WriteLine("Number5 is {0}", number5);   //Ondalıklı sayıları tutmak için kullanılan veri tipidir.
+
+//DECIMAL
+decimal number6 = 10.00000000007M;
+Console.WriteLine("Number6 is {0}", number6); // Virgülden sonra ki basamak sayısı artar ve genelde parasal değerler için kullanılır.
+
+
+
+
+
+
+
+
+
+
+Console.ReadLine();
+
