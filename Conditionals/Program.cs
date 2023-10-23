55fc67510d12fe38d8df4a404a29dcd836772024
Burak Tan <buraaktan78@gmail.com>
2023-10-24 02:43:02 +0300

    If_Else

---
+++ Conditionals/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//IF Bloklarıyla Çalışmak...
+var number = 7;
+if (number==7)
+{
+    Console.WriteLine("Number is 7");
+}
+else
+{
+    Console.WriteLine("Number is not 7");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Console.ReadLine();
