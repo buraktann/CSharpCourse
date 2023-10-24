6fa26641c50c67c6fdc598aecebfbaebfeaea713
Burak Tan <buraaktan78@gmail.com>
2023-10-24 22:17:32 +0300

    For

---
+++ Loops/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+for (int i = 100; i >= 0; i=i-2)
+{
+    Console.WriteLine(i);
+}
+Console.WriteLine("Finished");
+Console.ReadLine();
