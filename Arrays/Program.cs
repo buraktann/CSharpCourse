10138af6226b4cfdcd60221a328691fdbe4df3f4
Burak Tan <buraaktan78@gmail.com>
2023-10-24 22:10:03 +0300

    Multidimensional_Arrays

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 string[] students = new string[3] { "Burak", "Tan", "Engin" };
 
-string[] students2 = {"Burak","Tan","Engin" }
+string[] students2 = { "Burak", "Tan", "Engin" }
 
 ;
 foreach (string student in students)
@@ -9,7 +9,24 @@ foreach (string student in students)
     Console.WriteLine(student);
 }
 
+string[,] regions = new string[5, 3]
+{
+    {"Edirne","Kırklareli","Tekirdağ"},
+    {"Ankara","Konya","Kırıkkale"},
+    {"Antalya","Adana","Mersin"},
+    {"Rize","Trabzon","Samsun"},
+    {"İzmir","Muğla","Manisa"},
+};
 
+
+for (int i = 0; i <= regions.GetUpperBound(0); i++)
+{
+    for (int j = 0; j <= regions.GetUpperBound(1); j++)
+    {
+        Console.WriteLine(regions[i, j]);
+    }
+    Console.WriteLine("************");
+}
 
 
 Console.ReadLine();
