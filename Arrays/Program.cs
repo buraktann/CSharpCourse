72d53c698fe4474318b59b54ffa3fd53932943ce
Burak Tan <buraaktan78@gmail.com>
2023-10-24 21:59:20 +0300

    Array

---
+++ Arrays/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] students = new string[3] { "Burak", "Tan", "Engin" };
+
+string[] students2 = {"Burak","Tan","Engin" }
+
+;
+foreach (string student in students)
+{
+    Console.WriteLine(student);
+}
+
+
+
+
+Console.ReadLine();
