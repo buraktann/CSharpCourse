1ef5a50bddb7cd84e4af32f758a4d0095856917b
Burak Tan <buraaktan78@gmail.com>
2023-10-24 22:44:36 +0300

    String

---
+++ Strings/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string city = "Ankara";
+//Console.WriteLine(city[0]);
+
+foreach (var item in city)
+{
+    Console.WriteLine(item);
+}
+
+string city2 = "İstanbul";
+
+Console.WriteLine(String.Format("{0} {1}",city,city2));
+
+Console.ReadLine();
