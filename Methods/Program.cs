0d5999b279b34b4cc7ae070fc46f87c8a3a72dda
Burak Tan <buraaktan78@gmail.com>
2023-10-24 18:59:33 +0300

    Out_Keyword

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -6,7 +6,7 @@
 //var result = Add2();
 int number1 = 20;
 int number2 = 100;
-var result2 = Add3(ref number1, number2);
+var result2 = Add3(out number1, number2);
 Console.WriteLine(result2);
 Console.WriteLine(number1);
 Console.ReadLine();
@@ -20,7 +20,7 @@ Console.ReadLine();
 //    var result = number1 + number2;
 //    return result;
 //}
-static int Add3(ref int number1, int number2)
+static int Add3(out int number1, int number2) // Out ile Ref çok benzerdir tek farkı ref kullanırken önceki değişkene kesinlikle değer atanması gerekir Out için atmana gerek yoktur. Methodun içinde set edilmesi gerekir.
 {
     number1 = 30;
     return number1 + number2;
