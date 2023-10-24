70168c848d3971def807590721666a900ea3ee4a
Burak Tan <buraaktan78@gmail.com>
2023-10-24 03:01:48 +0300

    Multiple_Terms

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -45,22 +45,20 @@ switch (number)
         Console.WriteLine("Number is not 7 or 20");
         break;
 
-
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
+// Çoklu Şartlarla çalışmak...
+if (number >= 0 && number <=100)
+{
+    Console.WriteLine("Number is between 0-100");
+}
+else if (number > 100 && number <= 200)
+{
+    Console.WriteLine("Number is between 101-200");
+}
+else if (number > 200 || number < 0)
+{
+    Console.WriteLine("Number is less than 0 or greater than 200");
+}
 
 
 
