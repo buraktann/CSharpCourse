5e308902839809f7587b073507fd191fdff519c5
Burak Tan <buraaktan78@gmail.com>
2023-10-27 02:08:07 +0300

    Interface

---
+++ Interfaces/ICustomerDal.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    interface ICustomerDal
+    {
+        void Add();
+        void Update();
+        void Delete();
+
+    }
+
+
+
+    class SqlServerCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Sql Added!");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Sql Uptaded!");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Sql Deleted!");
+        }
+
+        
+    }
+
+    class OracleCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Oracle Added!");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Oracle Uptaded!");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Oracle Deleted!");
+        }
+
+    }
+
+    class CustomerManager
+    {
+        public void Add(ICustomerDal customerDal)
+        {
+            customerDal.Add();
+        }
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Interfaces;
+//InterfacesIntro();
+
+CustomerManager customerManager = new CustomerManager();
+customerManager.Add(new SqlServerCustomerDal());
+
+Console.ReadLine();
+
+static void InterfacesIntro()
+{
+    PersonManager manager = new PersonManager();
+
+    manager.Add(new Customer { Id = 1, FirstName = "Burak", LastName = "Tan", Address = "Kırklareli" });
+
+    Student student = new Student
+    {
+        Id = 1,
+        FirstName = "Yunus Emre",
+        LastName = "Tan",
+        Departmant = "Computer Sciences"
+
+    };
+    manager.Add(student);
+}
+
+interface IPerson
+{
+   public int Id { get; set; }
+   public string FirstName { get; set; }
+   public string LastName { get; set; }
+
+}
+
+class Customer : IPerson
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Address { get; set; }
+}
+
+class Student : IPerson
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Departmant { get; set; }
+}
+class Worker : IPerson
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Departmant { get; set; }
+}
+
+class PersonManager
+{
+    public void Add (IPerson person)
+    {
+        Console.WriteLine(person.FirstName);
+    }
+
+}
