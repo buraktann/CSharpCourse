cd458f46b40820bcbb4ebf91af03994762ff6c5b
Burak Tan <buraaktan78@gmail.com>
2023-10-27 02:44:17 +0300

    Inheritance

---
+++ Inheritance/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Person[] persons = new Person[3]
+    {
+        new Customer{FirstName="Burak"},
+        new Student{FirstName="Ali Güner"},
+        new Person{FirstName="Yunus Emre"}
+    };
+foreach (Person person in persons)
+{
+    Console.WriteLine(person.FirstName);
+}
+
+
+Console.ReadLine();
+
+class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+}
+
+class Customer : Person
+{
+    public string City { get; set; }
+}
+
+class Student : Person
+{
+    public string Department { get; set; }
+}
